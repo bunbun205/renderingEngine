ea087f4cbb0f0a5894c4a9eb4508964aeaf9ec5a
Beany <mayank69123@gmaail.com>
2021-08-18 12:12:45 +0530

    inlined functions in the headers

--- math/normal.hpp
+++ math/normal.hpp
@@ -30,19 +30,76 @@ public:
 typedef Normal3<double> Normal3f;
 typedef Normal3<int>    Normal3i;
 
-// 3D Normal inline function prototypes
+// 3D Normal inline functions
 
-template<typename T> std::ostream &operator << (std::ostream &out, const Normal3<T> &n);
-template<typename T> Vector3<T> operator + (const Normal3<T> &n, const Vector3<T> &v);
-template<typename T> Vector3<T> operator + (const Vector3<T> &v, const Normal3<T> &n);
-template<typename T> Vector3<T> operator - (const Normal3<T> &n, const Vector3<T> &v);
-template<typename T> Vector3<T> operator - (const Vector3<T> &v, const Normal3<T> &n);
-template<typename T> Normal3<T> operator * (T s, const Normal3<T> &n);
-template<typename T> Normal3<T> operator * (const Normal3<T> &n, T s);
-template<typename T> Normal3<T> operator / (const Normal3<T> &n, T s);
-template<typename T> T dot(const Normal3<T> &n, const Vector3<T> &v);
-template<typename T> Vector3<T> cross(const Normal3<T> &n, const Vector3<T> &v);
-template<typename T> T dot(const Vector3<T> &v, const Normal3<T> &n);
-template<typename T> Vector3<T> cross(const Vector3<T> &v, const Normal3<T> &n);
-template<typename T> Normal3<T> normalize(Normal3<T> n);
-template<typename T> Normal3<T> faceForward(const Normal3<T> &n, const Vector3<T> &v);
+template<typename T> inline std::ostream &operator << (std::ostream &out, const Normal3<T> &n) {
+
+        return out << n.x << ' ' << n.y << ' ' << n.z;
+}
+
+template<typename T> inline Vector3<T> operator + (const Normal3<T> &n, const Vector3<T> &v) {
+
+        return Vector3<T>(n.x + v.x, n.y + v.y, n.z + v.z);
+}
+
+template<typename T> inline Vector3<T> operator + (const Vector3<T> &v, const Normal3<T> &n) {
+
+        return n + v;
+}
+
+template<typename T> inline Vector3<T> operator - (const Normal3<T> &n, const Vector3<T> &v) {
+
+        return Vector3<T>(n.x - v.x, n.y - v.y, n.z - v.z);
+}
+
+template<typename T> inline Vector3<T> operator - (const Vector3<T> &v, const Normal3<T> &n) {
+
+        return Vector3<T>(v.x - n.x, v.y - n.y, v.z - n.z);
+}
+
+template<typename T> inline Normal3<T> operator * (T s, const Normal3<T> &n) {
+
+        return Normal3<T>(s * n.x, s * n.y, s * n.z);
+}
+
+template<typename T> inline Normal3<T> operator * (const Normal3<T> &n, T s) {
+
+        return s * n;
+}
+
+template<typename T> inline Normal3<T> operator / (const Normal3<T> &n, T s) {
+
+        return (1 / s) * n;
+}
+
+template<typename T> inline T dot(const Normal3<T> &n, const Vector3<T> &v) {
+
+        return n.x * v.x + n.y * v.y + n.z * v.z;
+}
+
+template<typename T> inline Vector3<T> cross(const Normal3<T> &n, const Vector3<T> &v) {
+
+        return Vector3<T>(n.y * v.z - n.z * v.y,
+                          n.z * v*x - n.x * v.z,
+                          n.x * v.y - n.y * v.x);
+}
+
+template<typename T> inline T dot(const Vector3<T> &v, const Normal3<T> &n) {
+
+        return dot(n, v);
+}
+
+template<typename T> inline Vector3<T> cross(const Vector3<T> &v, const Normal3<T> &n) {
+
+        return -cross(n, v);
+}
+
+template<typename T> inline Normal3<T> normalize(Normal3<T> n) {
+
+        return n / n.length();
+}
+
+template<typename T> inline Normal3<T> faceForward(const Normal3<T> &n, const Vector3<T> &v) {
+
+        return (dot(n, v) < 0.T) ? -n : n;
+}
--- math/vector.hpp
+++ math/vector.hpp
@@ -34,19 +34,57 @@ public:
 typedef Vector2<double> Vector2f;
 typedef Vector2<int>    Vector2i;
 
-// 2D Vector inline function prototypes
+// 2D Vector inline fuctions definition
 
-template<typename T> std::ostream &operator << (std::ostream &out, const Vector2<T> &v);
-template<typename T> Vector2<T> operator + (const Vector2<T> &u, const Vector2<T> &v);
-template<typename T> Vector2<T> operator - (const Vector2<T> &u, const Vector2<T> &v);
-template<typename T> Vector2<T> operator * (T s, const Vector2<T> &v);
-template<typename T> Vector2<T> operator * (const Vector2<T> &v, T s);
-template<typename T> Vector2<T> operator * (const Vector2<T> &v, Matrix<T, 2> &m);
-template<typename T> Vector2<T> operator * (Matrix<T, 2> &m, const Vector2<T> &v);
-template<typename T> Vector2<T> operator / (const Vector2<T> &v, T s);
-template<typename T> T dot(const Vector2<T> &u, const Vector2<T> &v);
-template<typename T> Vector2<T> cross(const Vector2<T> &u, const Vector2<T> &v);
-template<typename T> Vector2<T> normalize(Vector2<T> v);
+template<typename T> inline std::ostream &operator << (std::ostream &out, const Vector2<T> &v) {
+
+        return out << x << ' ' << y;
+}
+
+template<typename T> inline Vector2<T> operator + (const Vector2<T> &u, const Vector2<T> &v) {
+
+        return Vector2<T>(u.x + v.x, u.y + v.y);
+}
+
+template<typename T> inline Vector2<T> operator - (const Vector2<T> &u, const Vector2<T> &v) {
+
+        return Vector2<T>(u.x - v.x, u.y - v.y);
+}
+
+template<typename T> inline Vector2<T> operator * (T s, const Vector2<T> &v) {
+
+        return Vector2<T>(s * v.x, s * v.y);
+}
+
+template<typename T> inline Vector2<T> operator * (const Vector2<T> &v, T s) {
+
+        return s * v;
+}
+
+template<typename T> inline Vector2<T> operator * (const Vector2<T> &v, Matrix<T, 2> &m) {
+
+        return Vector2<T>(v.x * (m(0, 0) + m(1, 0)), v.y * (m(0, 1) + m(1, 1)));
+}
+
+template<typename T> inline Vector2<T> operator * (Matrix<T, 2> &m, const Vector2<T> &v) {
+
+        return Vector2<T>(v.x * (m(0, 0) + m(0, 1)), v.y * (m(1, 0) + m(1, 1)));
+}
+
+template<typename T> inline Vector2<T> operator / (const Vector2<T> &v, T s) {
+
+        return (1 / s) * v;
+}
+
+template<typename T> inline T dot(const Vector2<T> &u, const Vector2<T> &v) {
+
+        return u.x * v.x + u.y * v.y;
+}
+
+template<typename T> inline Vector2<T> normalize(Vector2<T> v) {
+
+        return v / v.length();
+}
 
 /**********************************************************************3D VECTOR**********************************************************************/
 
@@ -78,16 +116,61 @@ public:
 typedef Vector3<double> Vector3f;
 typedef Vector3<int>    Vector3i;
 
-// 3D Vector inline function prototypes
+// 3D Vector inline functions definition
 
-template<typename T> std::ostream &operator << (std::ostream &out, const Vector3<T> &v);
-template<typename T> Vector3<T> operator + (const Vector3<T> &u, const Vector3<T> &v);
-template<typename T> Vector3<T> operator - (const Vector3<T> &u, const Vector3<T> &v);
-template<typename T> Vector3<T> operator * (T s, const Vector3<T> &v);
-template<typename T> Vector3<T> operator * (const Vector3<T> &v, T s);
-template<typename T> Vector3<T> operator * (const Vector3<T> &v, Matrix<T, 3> &m);
-template<typename T> Vector3<T> operator * (Matrix<T, 3> &m, const Vector3<T> &v);
-template<typename T> Vector3<T> operator / (const Vector3<T> &v, T s);
-template<typename T> T dot(const Vector3<T> &u, const Vector3<T> &v);
-template<typename T> Vector3<T> cross(const Vector3<T> &u, const Vector3<T> &v);
-template<typename T> Vector3<T> normalize(Vector3<T> v);
+template<typename T> inline std::ostream &operator << (std::ostream &out, const Vector3<T> &v) {
+
+        return out << v.x << ' ' << v.y << ' ' << v.z;
+}
+
+template<typename T> inline Vector3<T> operator + (const Vector3<T> &u, const Vector3<T> &v) {
+
+        return Vector3<T>(u.x + v.x, u.y + v.y, u.z + v.z);
+}
+
+template<typename T> inline Vector3<T> operator - (const Vector3<T> &u, const Vector3<T> &v) {
+
+        return Vector3<T>(u.x - v.x, u.y - v.y, u.z - v.z);
+}
+
+template<typename T> inline Vector3<T> operator * (T s, const Vector3<T> &v) {
+
+        return Vector3<T>(s * v.x, s * v.y, s * v.z);
+}
+
+template<typename T> inline Vector3<T> operator * (const Vector3<T> &v, T s) {
+
+        return s * v;
+}
+
+template<typename T> inline Vector3<T> operator * (const Vector3<T> &v, Matrix<T, 3> &m) {
+
+        return Vector2<T>(v.x * (m(0, 0) + m(1, 0 + m(2, 0))), v.y * (m(0, 1) + m(1, 1) + m(2, 1)), v.z * (m(0, 2) + m(1, 2) + m(2, 2)));
+}
+
+template<typename T> inline Vector2<T> operator * (Matrix<T, 2> &m, const Vector2<T> &v) {
+
+        return Vector2<T>(v.x * (m(0, 0) + m(0, 1) + m(0, 2)), v.y * (m(1, 0) + m(1, 1) + m(1, 2)), v.z * (m(2, 0) + m(2, 1) + m(2, 2)));
+}
+
+template<typename T> inline Vector3<T> operator / (const Vector3<T> &v, T s) {
+
+        return (1 / s) * v;
+}
+
+template<typename T> inline T dot(const Vector3<T> &u, const Vector3<T> &v) {
+
+        return u.x * v.x + u.y * v.y + u.z * v.z;
+}
+
+template<typename T> inline Vector3<T> cross(const Vector3<T> &u, const Vector3<T> &v) {
+
+        return Vector3<T>(u.y * v.z - u.z * v.y,
+                          u.z * v*x - u.x * v.z,
+                          u.x * v.y - u.y * v.x);
+}
+
+template<typename T> inline Vector3<T> normalize(Vector3<T> v) {
+
+        return v / v.length();
+}
